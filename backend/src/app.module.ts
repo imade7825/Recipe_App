007d94a3bbf435a573736b0056cc8d87cf8dd574
Imade <elbahit@gmx.de>
2026-01-06 12:35:34 +0100

    chore: configure nestjs backend and postgresql connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,37 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config'; //loads .env into process.env
+import { TypeOrmModule } from '@nestjs/typeorm'; //Enables TypeORM DB connection
+import { RecipesModule } from './recipes/recipes.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    //makes .env variables available everywhere (no need to import again)
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    // connect nestjs to sql database using values form .env
+    TypeOrmModule.forRoot({
+      type: 'postgres', //we use PostgreSQL
+
+      //connection values come from .env
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+
+      //auto load entities registred in feature modules
+      autoLoadEntities: true,
+
+      //keep false: migrations will control schema changes
+      synchronize: false,
+
+      //helpful while learning: prints sql / connection info
+      logging: true,
+    }),
+    //our feature module
+    RecipesModule,
+  ],
 })
 export class AppModule {}
